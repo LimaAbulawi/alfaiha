0aa9efbcad1fb1d81f040b13e03164cd8a1bec44
lima <lima.98@yahoo.com>
2022-10-03 11:41:43 +0300

    secound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ProductsGalleryComponent } from './components/products-gallery/products-gallery.component';
+import { WhoAreWeComponent } from './components/who-are-we/who-are-we.component';
+import { TermsConditionsComponent } from './components/terms-conditions/terms-conditions.component';
+import { NewsComponent } from './components/shared/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductsGalleryComponent,
+    WhoAreWeComponent,
+    TermsConditionsComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
