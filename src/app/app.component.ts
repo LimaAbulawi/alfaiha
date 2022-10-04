1f6b09dcb3b049a36e79f17176196e5556bc7a54
lima <lima.98@yahoo.com>
2022-10-04 11:08:47 +0300

    22 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alfaiha';
+
+  
+  isScrollerd:boolean = false;
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    //In chrome and some browser scroll is given to body tag
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) ;
+    
+    // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+    if (pos >= 200) {
+      //Do your action here
+      this.isScrollerd = true;
+    }else{
+      this.isScrollerd = false;
+    }
+  }
 }
