01691ffe83ea55f23fe6e13d97e8b92734510bc8
lima <lima.98@yahoo.com>
2022-12-05 14:09:11 +0200

    changes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -54,7 +54,7 @@ export class MainComponent implements OnInit {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     variableWidth: false,
     pauseOnFocus: true
